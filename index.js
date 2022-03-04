a78201a8be2ebb4e887c9e2713f52ff9d1fcf340
Alessandro Solbiati <lessandro@fb.com>
2022-03-04 14:56:37 +0000

    [index.js] callback

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("%c  GAME ON THE CHROME DEV TOOLS", "color:blue; font-size:50px");
 console.log("%c  Click COMPUTE to start hacking", "color:red; font-size:40px");
-function callback(){const e=document.getElementById("A").innerHTML,n=document.getElementById("B").innerHTML,t=document.getElementById("C").innerHTML,m=Number(e)+Number(n)==Number(t)?"green":"red";document.getElementById("game").style.backgroundColor=m}
+function callback(){const e=document.getElementById("A").innerHTML,n=document.getElementById("B").innerHTML,t=document.getElementById("C").innerHTML,m=Number(e)+Number(n)==Number(t)?"green":"red",r=Number(e)+Number(n)==Number(t)?"CORRECT: ":"WRONG: ";document.getElementById("game").style.backgroundColor=m,alert(r+e+" + "+n+" = "+t),document.getElementById("A").innerHTML=Math.floor(1e7*Math.random()),document.getElementById("B").innerHTML=Math.floor(1e7*Math.random())}
 
 var stop = true;
 
@@ -16,7 +16,7 @@ function compute(){
 	// 1) Press "CMD + \" 
 	// 2) write the correct result 
 	// 3) Press "CMD + S"
-	const result = 14;
+	const result = 0;
 
 	document.getElementById("C").innerHTML = result;
 	callback();
